8ae42254f37a10257b23a1151c6006b0a9904f6b
Anton <132255178+dxfh00@users.noreply.github.com>
2024-07-24 11:06:32 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+homework=12
+hour=1.5
+course_name='Python'
+time=(hour/homework)
+print('Курс: '+str(course_name)+', всего задач:'+str(homework)+', затрачено часов:',str(hour)+
+      ', среднее время выполения',time,'часа.')
